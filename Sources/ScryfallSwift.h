51bd1b497198665dae586f2b80c5d5706953e109
Roudique <roudique@gmail.com>
2018-05-08 22:48:53 -0400

    Change framework name to comply with cocoapod

--- Sources/ScryfallSwift.h
+++ Sources/ScryfallSwift.h
@@ -15,4 +15,3 @@ FOUNDATION_EXPORT const unsigned char ScryfallSwift_VersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ScryfallSwift/PublicHeader.h>
 
-
