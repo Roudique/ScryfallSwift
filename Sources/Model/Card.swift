acddeaab33638c0c406748ec11f25c4275e95e60
Roudique <roudique@gmail.com>
2018-05-09 22:19:27 -0400

    Add CardFace entity

--- Sources/Model/Card.swift
+++ Sources/Model/Card.swift
@@ -29,6 +29,34 @@ public class Card: Decodable {
         case scryfallURI = "scryfall_uri"
         case printsSearchURI = "prints_search_uri"
         case rulingsURI = "rulings_uri"
+
+struct CardFace: Decodable {
+    var name: String
+    var typeLine: String
+    var oracleText: String?
+    var manaCost: String
+    var colors: [CardColor]
+    var colorIndicator: [CardColor]?
+    var power: String?
+    var toughness: String?
+    var loyalty: String?
+    var flavorText: String?
+    var illustrationID: String?
+    var imageURIs: [String]?
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case typeLine = "type_line"
+        case oracleText = "oracle_text"
+        case manaCost = "mana_cost"
+        case colors
+        case colorIndicator = "color_indicator"
+        case power
+        case toughness
+        case loyalty
+        case flavorText = "flavor_text"
+        case illustrationID = "illustration_id"
+        case imageURIs = "image_uris"
     }
 }
 
