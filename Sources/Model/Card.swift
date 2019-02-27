a75ea036aead36a74a54cb5732d3fc4a2b259b3f
Roudique <roudique@gmail.com>
2019-02-26 21:55:08 -0500

    Reorganize Card entity, add todos, fix CardFace, fix RelatedCard

--- Sources/Model/Card.swift
+++ Sources/Model/Card.swift
@@ -31,23 +31,20 @@ public class Card: Decodable {
     /// A language code for this printing. [Link to scryfall.com](https://scryfall.com/docs/api/languages)
     var lang: String
     
-    /// A unique ID for this card’s oracle identity. This value is consistent across reprinted card editions, and unique among different cards with the same name (tokens, Unstable variants, etc).
-    var oracleID: String
-    
-    /// This card’s multiverse IDs on Gatherer, if any, as an array of integers. Note that Scryfall includes many promo cards, tokens, and other esoteric objects that do not have these identifiers.
-    var multiverseIDs: [Int]?
-    
     /// This card’s Magic Online ID (also known as the Catalog ID), if any. A large percentage of cards are not available on Magic Online and do not have this ID.
     var mtgoID: Int?
     
     /// This card’s foil Magic Online ID (also known as the Catalog ID), if any. A large percentage of cards are not available on Magic Online and do not have this ID.
     var mtgoFoilID: Int?
     
-    /// A link to this card object on Scryfall’s API.
-    var uri: URL
+    /// This card’s multiverse IDs on Gatherer, if any, as an array of integers. Note that Scryfall includes many promo cards, tokens, and other esoteric objects that do not have these identifiers.
+    var multiverseIDs: [Int]?
+
+    /// This card’s ID on TCGplayer’s API, also known as the productId.
+    var tcgPlayerID: Int?
     
-    /// A link to this card’s permapage on Scryfall’s website.
-    var scryfallURI: URL
+    /// A unique ID for this card’s oracle identity. This value is consistent across reprinted card editions, and unique among different cards with the same name (tokens, Unstable variants, etc).
+    var oracleID: String
     
     /// A link to where you can begin paginating all re/prints for this card on Scryfall’s API.
     var printsSearchURI: URL
@@ -55,45 +52,23 @@ public class Card: Decodable {
     /// A link to this card’s rulings on Scryfall’s API.
     var rulingsURI: URL
     
+    /// A link to this card’s permapage on Scryfall’s website.
+    var scryfallURI: URL
+
+    /// A link to this card object on Scryfall’s API.
+    var uri: URL
+    
     
     //MARK: - Gameplay fields
     
-    /// The name of this card. If this card has multiple faces, this field will contain both names separated by ␣//␣.
-    var name: String
+    /// If this card is closely related to other cards, this property will be an array with related cards.
+    var allParts: [RelatedCard]?
     
-    /// A computer-readable designation for this card’s layout. 
-    var layout: Layout
+    /// An array of Card Face objects, if this card is multifaced.
+    var cardFaces: [CardFace]?
     
     /// The card’s converted mana cost. Note that some funny cards have fractional mana costs.
     var cmc: Double
-    
-    /// The type line of this card.
-    ///
-    /// **Null** if card is double-faced.
-    var typeLine: String?
-    
-    /// The Oracle text for this card, if any.
-    var oracleText: String?
-    
-    /// The mana cost for this card. This value will be any empty string "" if the cost is absent. Remember that per the game rules, a missing mana cost and a mana cost of {0} are different values.
-    ///
-    /// **Null** if card is double-faced.
-    var manaCost: String?
-    
-    /// This card’s power, if any. Note that some cards have powers that are not numeric, such as *.
-    var power: String?
-
-    /// This card’s toughness, if any. Note that some cards have toughnesses that are not numeric, such as *.
-    var toughness: String?
-    
-    /// This loyalty if any. Note that some cards have loyalties that are not numeric, such as X.
-    var loyalty: String?
-    
-    /// This card’s life modifier, if it is Vanguard card. This value will contain a delta, such as +2.
-    var lifeModifier: String?
-    
-    /// This card’s hand modifier, if it is Vanguard card. This value will contain a delta, such as -1.
-    var handModifier: String?
     
     /// This card’s colors.
     ///
@@ -106,62 +81,108 @@ public class Card: Decodable {
     /// **Null** if card is double-faced.
     var colors: [CardColor]?
     
-    /// The colors in this card’s color indicator, if any. A null value for this field indicates the card does not have one.
-    var colorIndicator: [CardColor]?
-    
     /// This card’s color identity.
     var colorIdentity: [CardColor]
-    
-    /// If this card is closely related to other cards, this property will be an array with related cards.
-    var allParts: [RelatedCard]?
-    
-    /// An array of Card Face objects, if this card is multifaced.
-    var cardFaces: [CardFace]?
-    
-    /// An object describing the legality of this card.
-    var legalities: Legality
-    
-    /// True if this card is on the Reserved List.
-    var reserved: Bool
-    
-    /// True if this printing exists in a foil version.
-    var foil: Bool
-    
-    /// True if this printing exists in a nonfoil version.
-    var nonfoil: Bool
-    
-    /// True if this card is oversized.
-    var oversized: Bool
+
+    /// The colors in this card’s color indicator, if any. A null value for this field indicates the card does not have one.
+    var colorIndicator: [CardColor]?
     
     /// This card’s overall rank/popularity on EDHREC. Not all carsd are ranked.
     var edhrecRank: Int?
     
+    /// True if this printing exists in a foil version.
+    var foil: Bool
+    
+    /// This card’s hand modifier, if it is Vanguard card. This value will contain a delta, such as -1.
+    var handModifier: String?
+    
+    /// A computer-readable designation for this card’s layout. 
+    var layout: Layout
+    
+    /// An object describing the legality of this card.
+    var legalities: Legality
+    
+    /// This card’s life modifier, if it is Vanguard card. This value will contain a delta, such as +2.
+    var lifeModifier: String?
+
+    /// This loyalty if any. Note that some cards have loyalties that are not numeric, such as X.
+    var loyalty: String?
+    
+    /// The mana cost for this card. This value will be any empty string "" if the cost is absent. Remember that per the game rules, a missing mana cost and a mana cost of {0} are different values.
+    ///
+    /// **Null** if card is double-faced.
+    var manaCost: String?
+
+    /// The name of this card. If this card has multiple faces, this field will contain both names separated by ␣//␣.
+    var name: String
+    
+    /// True if this printing exists in a nonfoil version.
+    var nonfoil: Bool
+    
+    /// The Oracle text for this card, if any.
+    var oracleText: String?
+    
+    /// True if this card is oversized.
+    var oversized: Bool
+    
+    /// This card’s power, if any. Note that some cards have powers that are not numeric, such as *.
+    var power: String?
+    
+    /// True if this card is on the Reserved List.
+    var reserved: Bool
+
+    /// This card’s toughness, if any. Note that some cards have toughnesses that are not numeric, such as *.
+    var toughness: String?
+
+    /// The type line of this card.
+    ///
+    /// **Null** if card is double-faced.
+    var typeLine: String?
+    
     
     //MARK: - Print fields
     
-    /// This card’s set code.
-    var set: String
+    /// The name of the illustrator of this card. Newly spoiled cards may not have this field yet.
+    var artist: String?
     
-    /// This card’s full set name.
-    var setName: String
+    /// This card’s border color: black, borderless, gold, silver, or white.
+    var borderColor: String
     
     /// This card’s collector number. Note that collector numbers can contain non-numeric characters, such as letters or ★.
     var collectorNumber: String
     
-    /// A link to where you can begin paginating this card’s set on the Scryfall API.
-    var setSearchURI: URL
+    /// True if this is a digital card on Magic Online.
+    var isDigital: Bool
+
+    /// The flavor text, if any.
+    var flavorText: String?
+
+    // MARK: -
+    // TODO: Add frame effect
+    // MARK: -
     
-    /// A link to this card’s set on Scryfall’s website.
-    var scryfallSetURI: URL
+    /// This card’s frame layout.
+    var frame: String
     
-    /// An object listing available imagery for this card.
-    var imagery: Imagery?
+    /// True if this card’s artwork is larger than normal.
+    var isFullArt: Bool
+
+    // MARK: -
+    // TODO: Add games
+    // MARK: -
     
     /// True if this card’s imagery is high resolution.
     var hasHighresImage: Bool
     
-    /// True if this card is a reprint.
-    var isReprint: Bool
+    /// A unique identifier for the card artwork that remains consistent across reprints. Newly spoiled cards may not have this field yet.
+    var illustrationID: String?
+    
+    /// An object listing available imagery for this card.
+    var imagery: Imagery?
+
+    // MARK: -
+    // TODO: Add prices.
+    // MARK: -
     
     /// The localized name printed on this card.
     var printedName: String?
@@ -171,41 +192,51 @@ public class Card: Decodable {
     
     /// The localized type line printed on this card.
     var printedTypeLine: String?
-    
-    /// True if this is a digital card on Magic Online.
-    var isDigital: Bool
+
+    // MARK: -
+    // TODO: Add promo
+    // TODO: Add purchase URIs
+    // MARK: -
     
     /// This card’s rarity. One of common, uncommon, rare, or mythic.
     var rarity: Rarity
+    
+    // MARK: -
+    // TODO: Add related_uris
+    // TODO: Add released_at
+    // MARK: -
+    
+    /// True if this card is a reprint.
+    var isReprint: Bool
+    
+    /// A link to this card’s set on Scryfall’s website.
+    var scryfallSetURI: URL
+    
+    /// This card’s full set name.
+    var setName: String
 
-    /// The flavor text, if any.
-    var flavorText: String?
+    // MARK: -
+    // TODO: Add set_uri
+    // MARK: -
     
-    /// The name of the illustrator of this card. Newly spoiled cards may not have this field yet.
-    var artist: String?
+    /// This card’s set code.
+    var set: String
     
-    /// A unique identifier for the card artwork that remains consistent across reprints. Newly spoiled cards may not have this field yet.
-    var illustrationID: String?
+    /// A link to where you can begin paginating this card’s set on the Scryfall API.
+    var setSearchURI: URL
     
-    /// This card’s frame layout.
-    var frame: String
-    
-    /// True if this card’s artwork is larger than normal.
-    var isFullArt: Bool
-    
-    /// This card’s watermark, if any.
-    var watermark: String?
-    
-    /// This card’s border color: black, borderless, gold, silver, or white.
-    var borderColor: String
-    
+    // MARK: -
+    // TODO: change these to story_spotlight
     /// This card’s story spotlight number, if any.
     var storySpotlightNumber: Int?
     
     /// A URL to this cards’s story article, if any.
     var storySpotlightURI: String?
+    // MARK: - mark
     
-    
+    /// This card’s watermark, if any.
+    var watermark: String?
+
     
     // CodingKeys
     
@@ -277,32 +308,42 @@ public class Card: Decodable {
 //MARK: -
 /// Multiface cards have a card_faces property containing at least two Card Face objects.
 struct CardFace: Decodable {
-    var name: String
-    var typeLine: String
-    var oracleText: String?
-    var manaCost: String
-    var colors: [CardColor]?
+    var artist: String
     var colorIndicator: [CardColor]?
-    var power: String?
-    var toughness: String?
-    var loyalty: String?
+    var colors: [CardColor]?
     var flavorText: String?
     var illustrationID: String?
     var imagery: Imagery?
+    var loyalty: String?
+    var manaCost: String
+    var name: String
+    var oracleText: String?
+    var power: String?
+    var printedName: String?
+    var printedText: String?
+    var printedTypeLine: String?
+    var toughness: String?
+    var typeLine: String
+    var watermark: String?
     
     enum CodingKeys: String, CodingKey {
-        case name
-        case typeLine = "type_line"
-        case oracleText = "oracle_text"
-        case manaCost = "mana_cost"
-        case colors
+        case artist
         case colorIndicator = "color_indicator"
-        case power
-        case toughness
-        case loyalty
+        case colors
         case flavorText = "flavor_text"
         case illustrationID = "illustration_id"
         case imagery = "image_uris"
+        case loyalty
+        case manaCost = "mana_cost"
+        case name
+        case oracleText = "oracle_text"
+        case power
+        case printedName = "printed_name"
+        case printedText = "printed_text"
+        case printedTypeLine = "printed_type_line"
+        case toughness
+        case typeLine = "type_line"
+        case watermark
     }
 }
 
@@ -313,11 +354,25 @@ struct RelatedCard: Decodable {
     /// An unique ID for this card in Scryfall’s database.
     var id: String
     
+    // MARK: -
+    // TODO: Add component
+    // MARK: -
+    
     /// The name of this particular related card.
     var name: String
     
+    /// The type line of this card.
+    var typeLine: String
+    
     /// A URI where you can retrieve a full object describing this card on Scryfall’s API.
     var uri: URL
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case name
+        case typeLine = "type_line"
+        case uri
+    }
 }
 
 
