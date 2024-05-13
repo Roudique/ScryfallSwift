642821a7344f213220a6742961ea1423eda1de5e
Alex Rudavka <roudique@gmail.com>
2024-05-12 23:36:07 -0400

    fix: ScryfallError private properties

--- Sources/ScryfallSwift/API/ScryfallError.swift
+++ Sources/ScryfallSwift/API/ScryfallError.swift
@@ -9,27 +9,27 @@ import Foundation
 
 public struct ScryfallError: Error, Decodable {
     /// An integer HTTP status code for this error.
-    var status: Int
+    public var status: Int
     
     /// A computer-friendly string representing the appropriate HTTP status code.
-    var code: String
+    public var code: String
     
     /// A human-readable string explaining the error.
-    var details: String
+    public var details: String
     
     /// A computer-friendly string that provides additional context for the main error.
     ///
     /// For example, an endpoint many generate `HTTP 404` errors for different kinds of input. This field will provide a label for the specific kind of 404 failure, such as `ambiguous`.
-    var type: String?
+    public var type: String?
     
     /// If your input also generated non-failure warnings, they will be provided as human-readable strings in this array.
-    var warnings: [String]?
+    public var warnings: [String]?
 }
 
 public enum CommonError: Error {
     case invalidURL(String)
     
-    var localizedDescription: String {
+    public var localizedDescription: String {
         if case let CommonError.invalidURL(path) = self {
             return "Invalid path: \(path)"
         }
